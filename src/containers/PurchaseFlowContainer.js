cadf4cd46bc1e5cf85db20ae8655fa0ad6c904f3
Unknown <sdesmond46@gmail.com>
2018-01-17 00:17:03 -0800

    Dots!

--- src/containers/PurchaseFlowContainer.js
+++ src/containers/PurchaseFlowContainer.js
@@ -1,16 +1,30 @@
 import React, { Component, PropTypes } from 'react';
-import { Button, Grid, Row, PageHeader } from 'react-bootstrap';
+import { Button, Grid, Jumbotron, Row, PageHeader, Image } from 'react-bootstrap';
 import PurchaseModal from '../components/PurchaseModal';
+import { SVGRectImageBuilder } from '../components/RectImage';
+import StatusDots from '../components/StatusDots';
 
 export default class PurchaseFlowContainer extends Component {
   render() {
+    const imageHeight = 50;
+    const baseRect = { x: 0, y: 0, w: 250, h: 250, color: 'hsl(0, 0%, 90%)' };
+    const subRects = [Object.assign({}, this.props.rectToPurchase, { color: 'hsl(0, 0%, 40%)' })];
+
+    const builder = new SVGRectImageBuilder(imageHeight, imageHeight, baseRect, subRects);
+    const previewImage = `data:image/svg+xml;base64,${btoa(builder.output())}`;
+    const price = '3.22 gwei';
+
     return (
       <Grid>
-        <Row>
-          <PageHeader>
-            Purchase a Plot <small>{this.props.rectToPurchase.w} x {this.props.rectToPurchase.h}</small>
-            
+        <Row >
+          <PageHeader style={{textAlign: 'center', width: '100%', marginTop: '0'}}>
+            <span>Purchase Plot</span><small>{'        '}{price}{'        '}</small>
+            <Image thumbnail src={previewImage} />
           </PageHeader>
+          <StatusDots items={['Image', 'Website', 'Buyout', 'Summary']} currentIndex={1} />
+        </Row>
+        <Row>
+          
         </Row>
       </Grid>
     )
