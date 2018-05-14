677852d0873546a2534be4d60d20c59e410a1220
Unknown <sdesmond46@gmail.com>
2018-05-14 15:36:03 -0700

    It renders

--- src/components/HeatMapLegend.tsx
+++ src/components/HeatMapLegend.tsx
@@ -11,17 +11,22 @@ const colors = d3Palette.schemeRdYlGn[11].slice(0, 11);
 const styles: StyleRulesCallback = theme => ({
   root: {
     padding: theme.spacing.unit,
-    width: 220,
-    height: 38
+    paddingBottom: 0
+    // width: 220
   },
   gradient: {
-    width: '100%',
-    height: '100%',
-    background: `linear-gradient(to right, ${colors.join(' , ')})`,
-    border: 'solid',
-    borderColor: theme.palette.grey['600'],
-    borderWidth: 1
-  }
+    stroke: theme.palette.grey['800'],
+    strokeWidth: 1
+  },
+  legendSvg: {
+    width: 200,
+    height: 40,
+  },
+  tickLine: {
+    stroke: theme.palette.grey['800'],
+    strokeWidth: 2
+  },
+  labelFont: theme.typography.headline
 });
 
 export interface HeatMapLegendProps extends WithStyles {
@@ -37,10 +42,30 @@ class HeatMapLegend extends React.Component<HeatMapLegendProps> {
     // const value = new BigNumber(1).minus(aboveMin.div(priceRange));
     // const color = d3Palette.schemeRdYlGn[11];
 
+    const minLabel = this.props.minPrice;
+    const maxLabel = this.props.maxPrice;
+
     return (
       <Paper className={classes.root}>
-        <div className={classes.gradient}>
-        </div>
+        <svg className={classes.legendSvg} width="400" height="80" viewBox="-5 -5 405 85">
+          <defs>
+            <linearGradient id="colorGradient">
+              {colors.map((color, index) => (<stop offset={`${index * 100 / 11}%`} stopColor={color} key={index} />))}
+            </linearGradient>
+          </defs>
+          <rect className={classes.gradient} x={0} y={20} width={400} height={60} fill="url(#colorGradient)" />
+
+          <line className={classes.tickLine} x1={1} x2={1} y1={15} y2={20} />
+          <line className={classes.tickLine} x1={100} x2={100} y1={15} y2={20} />
+          <line className={classes.tickLine} x1={200} x2={200} y1={15} y2={20} />
+          <line className={classes.tickLine} x1={300} x2={300} y1={15} y2={20} />
+          <line className={classes.tickLine} x1={399} x2={399} y1={15} y2={20} />
+          <text className={classes.labelFont} alignmentBaseline="baseline" x="4" y="12" width="100" height="15">{minLabel}</text>
+          <text className={classes.labelFont} textAnchor="end" alignmentBaseline="baseline" x="396" y="12" width="100" height="15">{maxLabel}</text>
+        </svg>
+        <Typography variant="body1" align="center" gutterBottom={true}>
+          Price per Pixel
+        </Typography>
       </Paper>
     );
   }
