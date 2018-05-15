9c0ba77f2b934c5bf890bffb2834e1f5b969b63a
Unknown <sdesmond46@gmail.com>
2018-05-15 13:54:21 -0700

    Cleanup imports

--- src/components/LoadingStatus.tsx
+++ src/components/LoadingStatus.tsx
@@ -1,25 +1,8 @@
-import Help from '@material-ui/icons/Help';
-import Home from '@material-ui/icons/Home';
-import Notifications from '@material-ui/icons/Notifications';
-import Person from '@material-ui/icons/Person';
-import { Badge, CircularProgress, Snackbar, SnackbarContent } from 'material-ui';
-import ShoppingCart from 'material-ui-icons/ShoppingCart';
 import { withStyles, StyleRulesCallback, WithStyles } from 'material-ui/styles';
-import Button, { ButtonProps } from 'material-ui/Button';
-import Drawer from 'material-ui/Drawer';
 import Grid from 'material-ui/Grid';
-import Paper from 'material-ui/Paper';
 import { LinearProgress, LinearProgressProps } from 'material-ui/Progress';
-import { SvgIconProps } from 'material-ui/SvgIcon';
-import Tooltip from 'material-ui/Tooltip';
 import Typography from 'material-ui/Typography';
-import * as PropTypes from 'prop-types';
 import * as React from 'react';
-
-import * as Actions from '../actions';
-import { ContractInfo, PlotInfo, Rect } from '../models';
-
-import FloatingLogo from './FloatingLogo';
 
 const styles: StyleRulesCallback = theme => ({
   root: {
